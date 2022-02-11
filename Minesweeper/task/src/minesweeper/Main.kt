67c74bc891df6aab78f462fc6a81fabfd19df8bb
Ktuhlya <97504952+Ktuhlya@users.noreply.github.com>
2022-02-11 15:28:17 +0700

    221

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -2,9 +2,10 @@ package minesweeper
 
 import kotlin.random.Random
 import kotlin.random.nextInt
+import kotlin.system.exitProcess
 
 val field = MutableList(9){MutableList(9){"."} }
-var fieldFalse = mutableListOf<MutableList<String>>()
+var fieldFalse = MutableList(9){MutableList(9){"."} }
 val listMines = mutableListOf<String>()
 
 var amount = 0
@@ -18,8 +19,7 @@ fun main() {
     while (field.toString().count(){it.toString() == "X"} < amount)
     recMineList()
     drawDigit()
-    Game().printField(Game().copyField(field))
-
+    Game().printField(Game().copyFieldF(field))
 }
 
 fun recMineList() {
@@ -33,12 +33,9 @@ fun recMineList() {
 
 class Game() {
 
-
-    fun copyField(field: MutableList<MutableList<String>>):
+    fun copyFieldF(field: MutableList<MutableList<String>>):
             MutableList<MutableList<String>> {
 
-        fieldFalse.clear()
-        fieldFalse=MutableList(9){MutableList(9){"."} }
 
         for (i in field.indices) {
             for (j in field[i].indices) {
@@ -49,72 +46,172 @@ class Game() {
 
         for (i in fieldFalse.indices) {
             for (j in fieldFalse[i].indices) {
-                if (fieldFalse[i][j] == "X") {
-                    fieldFalse[i][j] = "."
-                }
+                if ((fieldFalse[i][j] == "X") || (fieldFalse.isDigit(i, j)))
+                fieldFalse[i][j]  = "."
+
             }
         }
         return fieldFalse
     }
 
-    fun printField(field: MutableList<MutableList<String>>) {
 
-        fieldFalse.add(
+
+
+    private fun copyField(field: MutableList<MutableList<String>>, x: Int = 0, y: Int = 0):
+            MutableList<MutableList<String>> {
+
+    //    fieldFalse.clear()
+     //   fieldFalse=MutableList(9){MutableList(9){"."} }
+
+
+        for (i in fieldFalse.indices) {
+            for (j in fieldFalse[i].indices) {
+                fieldFalse[i][j]= field[i][j]
+            }
+        }
+        return fieldFalse
+    }
+
+    fun printField(fieldIn: MutableList<MutableList<String>>) {
+
+
+        val fieldFalsePrint=MutableList(9){MutableList(9){"."} }
+
+        for (i in fieldIn.indices) {
+            for (j in fieldIn[i].indices) {
+                var temp = fieldIn[i][j]
+                fieldFalsePrint[i][j] = temp
+            }
+        }
+
+        fieldFalsePrint.add(
             0, mutableListOf(
                 " ", "|", "1", "2", "3", "4", "5", "6",
                 "7", "8", "", "9", "|"
             )
         )
-        fieldFalse.add(
+        fieldFalsePrint.add(
             1, mutableListOf(
                 "-", "|", "-", "-", "-", "-", "-",
                 "-", "-", "-", "-", "|"
             )
         )
         for (i in 2..10) {
-            fieldFalse[i].add(0, (i - 1).toString())
-            fieldFalse[i].add(1, "|")
-            fieldFalse[i].add(11, "|")
+            fieldFalsePrint[i].add(0, (i - 1).toString())
+            fieldFalsePrint[i].add(1, "|")
+            fieldFalsePrint[i].add(11, "|")
         }
-        fieldFalse.add(
+        fieldFalsePrint.add(
             11, mutableListOf(
                 "-", "|", "-", "-", "-", "-", "-",
                 "-", "-", "-", "-", "|"
             )
         )
-        for (i in fieldFalse.indices) println(fieldFalse[i].joinToString(""))
+        println()
+        for (i in fieldFalsePrint.indices)
+            println(fieldFalsePrint[i].joinToString(""))
         checkField()
-       // letsPlay()
     }
 
     private fun checkField() {
 
+        var checkList = mutableListOf<String>()
+        for (i in field.indices){
+            for (j in field[i].indices){
+                if (field[i][j] == "*") checkList.add("${i},${j}")
+            }
+        }
 
+        if (checkList.size == listMines.size){
+            for (i in checkList.indices){
+                if (checkList[i] != listMines[i]) letsPlay()
+                else{
+                    println("Congratulations! You found all the mines!")
+                    System.exit(0)
+                    break
+                }
+            }
+        } else letsPlay()
     }
 
     private fun letsPlay() {
-     //   for (i in field.indices) println(field[i].joinToString(""))
-        println("Set/delete mines marks (x and y coordinates): > ")
-        var (x, y) = readln().split(" ").map { it.toInt() }
-         when {
-             field.isDigit(x-1, y-1) -> {
-                 println("There is a number here!")
-                 letsPlay()
-             }
-             field.isMark(x-1, y-1) -> {
-                 field[x-1][y-1] = "."
-                 printField(copyField(field))
-             }
-             field.isMine(x-1, y-1) -> {
 
-             }
-             else -> {
-                 field[x-1][y-1] = "*"
-                 printField(copyField(field))
-             }
-         }
+        print("Set/unset mine marks or claim a cell as free: > ")
+
+        var (b, a, type) = readln().split(" ")
+        var x = a.toInt() - 1
+        var y = b.toInt() - 1
+        if (type == "mine") typeMine(x, y)
+        if (type == "free") typeFree(x, y)
+
     }
 
+    private fun typeFree(x: Int, y: Int) {
+
+        when {
+            (listMines.contains("$x,$y")) -> {
+                println("You stepped on a mine and failed!")
+                exitProcess(0)
+            }
+            (field.isDigit(x, y)) -> {
+                fieldFalse[x][y] = field[x][y]
+                printField(fieldFalse)
+            }
+            field[x][y] =="." -> {
+                fieldFalse[x][y] = "/"
+                checkAround(x, y)
+            }
+            else -> checkAround(x, y)
+            }
+        }
+
+    private fun checkAround(x: Int, y: Int) {
+        val listAround = mutableListOf<String>()
+      //  if ((x-1 != -1) && (x+1 != 9) && (y-1 != -1) && (y+1 != 9)){
+            listAround.add("${x-1},${y+1}")
+            listAround.add("${x-1},${y}")
+            listAround.add("${x-1},${y-1}")
+            listAround.add("${x},${y-1}")
+            listAround.add("${x+1},${y-1}")
+            listAround.add("${x+1},${y}")
+            listAround.add("${x+1},${y+1}")
+            listAround.add("${x},${y+1}")
+
+        listAround.removeIf { it.split(",").contains("-1") }
+        listAround.removeIf { it.split(",").contains("9") }
+     //   println(listAround)
+
+            for (i in listAround.indices){
+                var (r,c) = listAround[i].split(",")
+                if (field.isDigit(r.toInt(), c.toInt())) {
+                    fieldFalse[r.toInt()][c.toInt()] = field[r.toInt()][c.toInt()]
+                }
+                if (field[r.toInt()][c.toInt()] == ".") {
+                    fieldFalse[r.toInt()][c.toInt()] = "/"
+
+          //!!!!          checkAround(r.toInt(), c.toInt())
+
+                }
+
+
+/////////////// палку ставить надо на пустую
+        }
+        printField(fieldFalse)
+    }
+
+    private fun typeMine(x: Int, y: Int) {
+
+        when {
+            fieldFalse.isMark(x, y) -> {
+                fieldFalse[x][y] = "."
+                printField(fieldFalse)
+            }
+            else -> {
+                fieldFalse[x][y] = "*"
+                printField(fieldFalse)
+            }
+        }
+    }
 
 }
 fun MutableList<MutableList<String>>.isMine(row: Int, col: Int): Boolean {
@@ -159,15 +256,24 @@ fun drawDigit() {
 }
 
 
-
-
 /*
-  for (i in fieldFalse.indices){
-        for (j in fieldFalse[i].indices){
-            if (fieldFalse[i][j] == "X") {
-                fieldFalse[i][j] = "."
-                println("TTTT")
-            }
-        }
+    when {
+             field.isDigit(x-1, y-1) -> {
+                 println("There is a number here!")
+                 letsPlay()
+             }
+             field.isMark(x-1, y-1) -> {
+                 field[x-1][y-1] = "."
+                 printField(copyField(field))
+             }
+             field.isMine(x-1, y-1) -> {
+                field[x-1][y-1] = "*"
+                 printField(copyField(field))
+             }
+             else -> {
+                 field[x-1][y-1] = "*"
+                 printField(copyField(field))
+             }
+         }
     }
  */
